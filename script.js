f63e401e4e98da6a1ae271ac6ad6b8a97f47cb05
tributeTBC <134075185+tributeTBC@users.noreply.github.com>
2023-09-26 14:25:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function typeWriter(txt, outputElem) {
         if (index < txt.length && !stopTyping) {
             outputElem.textContent += txt.charAt(index);
             index++;
-            let timeoutId = setTimeout(typeChar, 1); // Adjust speed as needed
+            let timeoutId = setTimeout(typeChar, 0.5); // Adjust speed as needed
             currentTimeouts.push(timeoutId);
         }
     }
