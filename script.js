90262cdea98adfd801b06c0dc1061b6faf190456
tributeTBC <134075185+tributeTBC@users.noreply.github.com>
2023-09-26 15:02:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById("input").focus();
 });
 
-let currentTimeouts = [];
-let stopTyping = false;
-
 function typeWriter(txt, outputElem) {
-    //stopAllTyping();  // Clear any ongoing typing
     let index = 0;
     function typeChar() {
-        if (index < txt.length && !stopTyping) {
+        if (index < txt.length) {
             outputElem.textContent += txt.charAt(index);
             index++;
-            let timeoutId = setTimeout(typeChar, 50);
-            currentTimeouts.push(timeoutId);
+            setTimeout(typeChar, 50);
         }
     }
     typeChar();
@@ -34,7 +29,6 @@ document.getElementById("input").addEventListener("keydown", function(event) {
 
 document.getElementById("commands").addEventListener("click", function(event) {
     if (event.target.tagName === "SPAN") {
-        stopAllTyping();  // Interrupt any ongoing typing
         handleCommand(event.target.textContent);
         scrollToBottom();
     }
@@ -64,30 +58,22 @@ function handleCommand(command) {
             return;
     }
 
-    fetch(`./${fileName}.txt`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(content => {
-            typeWriter(content, document.getElementById("output"));
-            scrollToBottom();
-        })
-        .catch(err => {
-            typeWriter('\nError fetching file content!', document.getElementById("output"));
-            scrollToBottom();
-        });
+    fetch(`./${fileName}.txt`).then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    }).then(content => {
+        document.getElementById("output").textContent = ''; // Clear the output
+        typeWriter(content, document.getElementById("output"));
+        scrollToBottom();
+    }).catch(err => {
+        typeWriter('\nError fetching file content!', document.getElementById("output"));
+        scrollToBottom();
+    });
 }
 
 function scrollToBottom() {
     const output = document.getElementById("output");
     output.scrollTop = output.scrollHeight;
-}
-
-function stopAllTyping() {
-    stopTyping = true;
-    currentTimeouts.forEach(timeoutId => clearTimeout(timeoutId));
-    currentTimeouts = [];
 }
