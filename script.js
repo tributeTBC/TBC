973e4250d2956dfb5d9b7c4f55e980bd42dfb91f
tributeTBC <134075185+tributeTBC@users.noreply.github.com>
2023-10-02 15:30:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -227,35 +227,25 @@ function embedHTMLPage() {
   document.getElementById("output").appendChild(iframe);
   let lastHeight = 0; // variable to store the last known height of the iframe content
 
-  // Function to adjust the iframe height
   const adjustHeight = () => {
-    if (iframe.contentWindow && iframe.contentWindow.document) {
-      const currentHeight = iframe.contentWindow.document.body.scrollHeight;
-      const lineHeight = parseInt(
-        window.getComputedStyle(iframe.contentWindow.document.body).lineHeight,
-        10
-      );
-      const extraLines = 6; // Number of extra lines you want to add
-      const extraHeight = lineHeight * extraLines;
+    const currentHeight = iframe.contentWindow.document.body.scrollHeight;
 
-      if (lastHeight !== currentHeight) {
-        iframe.style.height = currentHeight + extraHeight + "px";
+    // Check if the height has changed
+    if (lastHeight !== currentHeight) {
+      const newHeight = currentHeight + 100;
 
-        // Scroll the #output area to the bottom
-        const outputDiv = document.getElementById("output");
-        outputDiv.scrollTop = outputDiv.scrollHeight;
+      iframe.style.height = newHeight + "px";
 
-        // Update lastHeight to the current height
-        lastHeight = currentHeight;
-      }
+      // Scroll the #output area to the bottom
+      const outputDiv = document.getElementById("output");
+      outputDiv.scrollTop = outputDiv.scrollHeight;
+      lastHeight = currentHeight + 100;
+      // Update lastHeight to the current height
     }
   };
 
-  // Adjust the height when the iframe loads
   iframe.onload = function () {
-    setTimeout(() => {
-      adjustHeight(); // Initial adjustment
-      setInterval(adjustHeight, 500); // Subsequent adjustments every 500 milliseconds
-    }, 1000); // Delay of 1 second to ensure iframe is fully loaded
+    adjustHeight();
+    setInterval(adjustHeight, 2000); // Check for height changes every half a second
   };
 }
